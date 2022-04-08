c2c81ea537ba94cea75e658d294a4706e25cb458
Laurence Lau <llau@LLAU.local>
2022-04-08 16:18:40 -0400

    test out this branch edit stuff

--- github.sql
+++ github.sql
@@ -1,4 +1,5 @@
-SELECT date(uaf.account_created_at_est) as account_created_date, count(*) as count_all_signups
+SELECT date(uaf.account_created_at_est) as account_created_date, count(*) as count_all_signups,
+count(uaf.registry_accoount_id) as count_registry
 FROM internal. user_account_facts uaf
 where uaf.account_created_at_est >= current_timestamp() - interval '3 days'
 group by 1
